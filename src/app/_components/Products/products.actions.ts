abb3be4590c2a9024d34d3f1c5052afbaf09d082
Moamen71244 <150190059+Moamen71244@users.noreply.github.com>
2026-06-28 00:48:26 +0300

    change v2 to v2

--- src/app/_components/Products/products.actions.ts
+++ src/app/_components/Products/products.actions.ts
@@ -1,24 +1,22 @@
 "use server";
 import { CartResponse } from "@/app/interfaces";
 import { DecodeToken } from "@/app/utilities";
-export async function addToCart(id:string){
-   const Token = await DecodeToken()
-   const bodyObj = {
-    productId: id
-   }
-    if (Token) {
-        try {
-            const res = await fetch(`https://ecommerce.routemisr.com/api/v2/cart`, {
-                method:"post",
-                headers:{token:Token, "Content-Type":"application/json"},
-                body:JSON.stringify(bodyObj)
-            }
-           )
-            const finalres = await res.json()
-            return finalres as CartResponse
-        } catch (error) {
-            console.log(error);
-        }
+export async function addToCart(id: string) {
+  const Token = await DecodeToken();
+  const bodyObj = {
+    productId: id,
+  };
+  if (Token) {
+    try {
+      const res = await fetch(`https://ecommerce.routemisr.com/api/v1/cart`, {
+        method: "post",
+        headers: { token: Token, "Content-Type": "application/json" },
+        body: JSON.stringify(bodyObj),
+      });
+      const finalres = await res.json();
+      return finalres as CartResponse;
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
-
